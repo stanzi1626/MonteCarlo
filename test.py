7b2275bbbf88001f7be310451a4be196be5ef7e4
stanzi1626 <stansfield_alex@yahoo.com>
2022-05-15 23:30:36 +0100

    removed multiprocessing (does not work)

--- test.py
+++ test.py
@@ -1,27 +1,20 @@
-import math
-import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+import multiprocessing
+import time
 
-fig = plt.figure(figsize=(4,4))
-ax = fig.add_subplot(111, projection='3d')
+def sleepy_man():
+    print('Starting to sleep')
+    time.sleep(1)
+    print('Done sleeping')
 
-xdata = np.zeros(0)
-ydata = np.zeros(0)
-zdata = np.zeros(0)
 
-for j in range(0, 1000):
-    x = np.random.uniform(-1, 1, 1)
-    y_limit = np.sqrt(1 - x**2)
-    y = np.random.uniform(-y_limit, y_limit, 1)
-    i = np.random.uniform(0, 1, 1)
-    if  i < 0.5:
-        z = np.sqrt(1-x**2-y**2)
-    else:
-        z = -np.sqrt(1-x**2-y**2)
-    xdata = np.append(xdata, x)
-    ydata = np.append(ydata, y)
-    zdata = np.append(zdata, z)
+if __name__ == '__main__':
+    tic = time.time()
+    p1 =  multiprocessing.Process(target= sleepy_man)
+    p2 =  multiprocessing.Process(target= sleepy_man)
+    p1.start()
+    p2.start()
+    p1.join()
+    p2.join()
+    toc = time.time()
 
-ax.scatter(xdata, ydata, zdata)
-plt.show()
+    print('Done in {:.4f} seconds'.format(toc-tic))
